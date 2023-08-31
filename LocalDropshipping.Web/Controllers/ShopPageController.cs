1a3af966928f6570c2d56548649ed25d612bf56b
Usama <usamahaseeb777@gmail.com>
2023-08-31 16:16:07 +0500

    Not-Complete

---
+++ LocalDropshipping.Web/Controllers/ShopPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LocalDropshipping.Web.Controllers
+{
+    public class ShopPageController : Controller
+    {
+        public IActionResult ShopPage()
+        {
+            return View();
+        }
+    }
+}
