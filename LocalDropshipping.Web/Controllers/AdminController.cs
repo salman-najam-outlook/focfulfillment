196ebf137b1df1bdab3db662fdc01a1f825ca906
Gullzaibkhan <ngullzaib9@gmail.com>
2023-09-01 10:54:39 +0500

    implemented-admin-dashboardTheme with some errors and bugs

---
+++ LocalDropshipping.Web/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LocalDropshipping.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult us7yhs6tdgv()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
