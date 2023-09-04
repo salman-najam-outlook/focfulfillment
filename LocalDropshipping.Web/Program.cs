04075b73cf6f78838ea1cb95f0080b99a4e986fc
ZU3AIRE <xubairjamil@gmail.com>
2023-09-04 12:58:04 +0500

    Added swagger UI for API testing

--- LocalDropshipping.Web/Program.cs
+++ LocalDropshipping.Web/Program.cs
@@ -3,6 +3,7 @@ using LocalDropshipping.Web.Data.Entities;
 using LocalDropshipping.Web.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -32,6 +33,12 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.SignIn.RequireConfirmedEmail = false;
 });
 
+// Swagger 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Foc Fulfilment", Version = "v1" });
+});
+
 
 
 // Services
@@ -53,6 +60,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// Swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Foc Fulfilment");
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
