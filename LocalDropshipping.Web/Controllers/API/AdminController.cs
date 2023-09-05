930f249c46cb0d4bae295691eb01aa6ad69f8390
@uar2971 <uar2971@gmail.com>
2023-09-05 13:36:32 +0500

    Controller Improvements Completed

---
+++ LocalDropshipping.Web/Controllers/API/AdminController.cs
@@ -0,0 +1,171 @@
+﻿using LocalDropshipping.Web.Data.Entities;
+using LocalDropshipping.Web.Dtos;
+using LocalDropshipping.Web.Enum;
+using LocalDropshipping.Web.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace LocalDropshipping.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IProductsService productService;
+        private readonly ICategoryService categoryService;
+        private readonly ISubscriptionsService sub;
+
+        public AdminController(IProductsService productService, ICategoryService categoryService, ISubscriptionsService Sub)
+        {
+            this.productService = productService;
+            this.categoryService = categoryService;
+            sub = Sub;
+        }
+
+        #region Product
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            return Ok(productService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            return Ok(productService.GetById(id));
+        }
+
+        [HttpPost]
+        public IActionResult PostProduct(ProductDto productDto)
+        {
+            Product product = productDto.ToEntity();
+            productService.Add(product);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            productService.Delete(id);
+            return Ok();
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult UpdateProduct(int id, ProductDto productDto)
+        {
+            Product product = productDto.ToEntity();
+            productService.Update(id, productDto);
+            return Ok();
+        }
+        [HttpGet]
+        public IActionResult GetProductbyPriceRange(decimal minPrice, decimal maxPrice)
+        {
+            var products = productService.GetProductsByPriceRange(minPrice, maxPrice);
+            if (products.Count == 0)
+            {
+                return NotFound("No Products Available");
+            }
+            return Ok(products);
+        }
+        #endregion
+
+        #region Category
+        [HttpGet]
+        public IActionResult GetAllCategories()
+
+        {
+            return Ok(categoryService.GetAll());
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategoryById(int id)
+        {
+            return Ok(categoryService.GetById(id));
+        }
+
+
+
+        [HttpPost]
+        public IActionResult AddCategory(CategoryDto categoryDto)
+        {
+            Category category = categoryDto.ToEntity();
+            categoryService.Add(category);
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCategory(int id)
+        {
+            categoryService.Delete(id)
+;
+            return Ok();
+        }
+        [HttpPost("{id}")]
+        public IActionResult UpdateCategory(int id, CategoryDto categoryDto)
+        {
+            Category category = categoryDto.ToEntity();
+            categoryService.Update(id, categoryDto);
+            return Ok();
+        }
+        #endregion
+
+        #region Subscriptions Todo(Zubair)
+        [HttpGet]
+        public IActionResult GetAllSubscriptions()
+
+        {
+            return Ok(sub.GetAll());
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetSubscriptionById(int id)
+        {
+            return Ok(sub.GetById(id));
+        }
+        [HttpPost]
+        public IActionResult AddSubscription(SubscriptionsDto subscriptionsDt)
+        {
+            Subscription Subb = subscriptionsDt.ToEntity();
+            sub.Add(Subb);
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteSubscription(int id)
+        {
+            sub.Delete(id)
+;
+            return Ok();
+        }
+        [HttpPost("{id}")]
+        public IActionResult UpdatePaymentStatus(int id, PaymentStatus newPaymentStatus)
+        {
+            var updatedPStatus = sub.UpdatePaymentStatus(id, newPaymentStatus);
+
+            if (updatedPStatus != null)
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult UpdateSubscriptionStatus(int id)
+        {
+            var updatedMembership = sub.UpdateSubscriptionStatus(id);
+
+            if (updatedMembership != null)
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        #endregion
+
+    }
+}
--- LocalDropshipping.Web/Controllers/AdminController.cs
+++ LocalDropshipping.Web/Controllers/AdminController.cs
@@ -6,106 +6,139 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LocalDropshipping.Web.Controllers
 {
-	public class AdminController : Controller
-	{
+    public class AdminController : Controller
+    {
 
-		private readonly IAdminService service;
+        private readonly IAdminService service;
+        private readonly IProductsService productsService;
+        private readonly ICategoryService categoryService;
 
-		public AdminController(IAdminService service)
-		{
-			this.service = service;
-		}
+        public AdminController(IAdminService service, IProductsService productsService, ICategoryService categoryService)
+        {
+            this.service = service;
+            this.productsService = productsService;
+            this.categoryService = categoryService;
+        }
+
+        #region Admin Login
+        public IActionResult AdminLogin()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public async Task<IActionResult> AdminLogin(AdminLoginViewModel model)
+        //{
+        //	if (ModelState.IsValid)
+        //	{
+        //		var result = await service.AdminLoginUser(model.Email, model.Password);
+
+        //		if (result.Succeeded)
+        //		{
+        //			// Check if the user is an admin
+        //			var isAdmin = await service.IsUserAdminAsync(model.Email);
+
+        //			if (isAdmin)
+        //			{
+        //				// Redirect to the admin dashboard if the user is an admin
+        //				return RedirectToAction("us7yhs6tdgv", "Admin");
+        //			}
+        //			else
+        //			{
+        //				// Handle non-admin user here, e.g., redirect to a different page
+        //				ModelState.AddModelError("", "OOPs. its seem like you're not an admin");
+        //				//	return RedirectToAction("NonAdminPage");
+        //			}
+        //		}
+
+        //		ModelState.AddModelError("", "Invalid username or password.");
+        //	}
+
+        //	return View(model);
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> AdminLogin(AdminLoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await service.AdminLoginUser(model.Email, model.Password);
+
+                if (result.Succeeded)
+                {
+                    // Check if the user is an admin
+                    var isAdmin = await service.IsUserAdminAsync(model.Email);
+
+                    // Check if the user is superadmin
+                    var isSuperAdmin = await service.IsUserSuperAdminAsync(model.Email);
+
+                    // Check if the user is active
+                    var isActive = await service.IsUserActiveAsync(model.Email);
+
+                    if (isAdmin)// || isSuperAdmin
+                    {
+                        if (isActive)
+                        {
+                            // Redirect to the admin dashboard if the user is an admin active
+                            return RedirectToAction("us7yhs6tdgv", "Admin");
+                        }
+                        else
+                        {
+                            // Handle inactive admin/superadmin
+                            ModelState.AddModelError("", "Your account is not active.");
+                        }
+                    }
+                    else
+                    {
+                        // Handle non-admin user here, e.g., redirect to a different page
+                        ModelState.AddModelError("", "Oops, it seems like you're not an admin.");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid username or password.");
+                }
+            }
+
+            return View(model);
+        }
 
 
-		public IActionResult AdminLogin()
-		{
-			return View();
-		}
-		//[HttpPost]
-		//public async Task<IActionResult> AdminLogin(AdminLoginViewModel model)
-		//{
-		//	if (ModelState.IsValid)
-		//	{
-		//		var result = await service.AdminLoginUser(model.Email, model.Password);
+        public ActionResult us7yhs6tdgv()
+        {
+            return View();
+        }
+        #endregion
 
-		//		if (result.Succeeded)
-		//		{
-		//			// Check if the user is an admin
-		//			var isAdmin = await service.IsUserAdminAsync(model.Email);
+        #region Products
+        public IActionResult GetAllProducts()
+        {
+            return View(productsService.GetAll());
+        }
 
-		//			if (isAdmin)
-		//			{
-		//				// Redirect to the admin dashboard if the user is an admin
-		//				return RedirectToAction("us7yhs6tdgv", "Admin");
-		//			}
-		//			else
-		//			{
-		//				// Handle non-admin user here, e.g., redirect to a different page
-		//				ModelState.AddModelError("", "OOPs. its seem like you're not an admin");
-		//				//	return RedirectToAction("NonAdminPage");
-		//			}
-		//		}
+        public IActionResult Post()
+        {
+            return View();
 
-		//		ModelState.AddModelError("", "Invalid username or password.");
-		//	}
+        }
+        [HttpPost]
+        public IActionResult SubmitForm(Product model)
+        {
+            if (model != null)
+            {
+                productsService.Add(model);
+                return RedirectToAction("GetAllProducts");
 
-		//	return View(model);
-		//}
+            }
+            return View("Post");
+        }
+        #endregion
 
-		[HttpPost]
-		public async Task<IActionResult> AdminLogin(AdminLoginViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				var result = await service.AdminLoginUser(model.Email, model.Password);
+        #region Categories
+        public IActionResult GetAllCategories()
+        {
+            return View(categoryService.GetAll());
+        }
+        #endregion
 
-				if (result.Succeeded)
-				{
-					// Check if the user is an admin
-					var isAdmin = await service.IsUserAdminAsync(model.Email);
-
-					// Check if the user is superadmin
-					var isSuperAdmin = await service.IsUserSuperAdminAsync(model.Email);
-
-					// Check if the user is active
-					var isActive = await service.IsUserActiveAsync(model.Email);
-
-					if (isAdmin)// || isSuperAdmin
-					{
-						if (isActive)
-						{
-							// Redirect to the admin dashboard if the user is an admin active
-							return RedirectToAction("us7yhs6tdgv", "Admin");
-						}
-						else
-						{
-							// Handle inactive admin/superadmin
-							ModelState.AddModelError("", "Your account is not active.");
-						}
-					}
-					else
-					{
-						// Handle non-admin user here, e.g., redirect to a different page
-						ModelState.AddModelError("", "Oops, it seems like you're not an admin.");
-					}
-				}
-				else
-				{
-					ModelState.AddModelError("", "Invalid username or password.");
-				}
-			}
-
-			return View(model);
-		}
-
-
-		public ActionResult us7yhs6tdgv()
-		{
-			return View();
-		}
-		public ActionResult Index()
-		{
-			return View();
-		}
-	}
+    }
 }
