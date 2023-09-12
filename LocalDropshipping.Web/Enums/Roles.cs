711d27cec9fc33853c44b0da46816532db5bdc1f
ZU3AIRE <xubairjamil@gmail.com>
2023-09-12 16:35:08 +0500

    Quickfix - updated roles enum

--- LocalDropshipping.Web/Enums/Roles.cs
+++ LocalDropshipping.Web/Enums/Roles.cs
@@ -1,9 +1,10 @@
 ﻿namespace LocalDropshipping.Web.Enums
 {
+    [Flags]
     public enum Roles
     {
-        SuperAdmin,
-        Admin,
-        Seller
+        SuperAdmin = 0b_0000_0001,
+        Admin = 0b_0000_0010,
+        Seller = 0b_0000_0100,
     }
 }
