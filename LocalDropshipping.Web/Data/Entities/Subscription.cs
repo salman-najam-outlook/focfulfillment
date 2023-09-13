ca9a4597912f1a5ec3494444ad74be3b4eb2d334
hamzakhan <hkhan8624@gmail.com>
2023-09-13 11:57:39 +0500

    Updated Subscription Entity

--- LocalDropshipping.Web/Data/Entities/Subscription.cs
+++ LocalDropshipping.Web/Data/Entities/Subscription.cs
@@ -13,8 +13,8 @@ namespace LocalDropshipping.Web.Data.Entities
 		public DateTime ActivationDate { get; set; }
 		public DateTime ExpiryDate { get; set; }
 		public DateTime CreatedDate { get; set; }
-		public int CreatedBy { get; set; }
+		public string CreatedBy { get; set; }
 		public DateTime UpdatedDate { get; set; }
-		public int UpdatedBy { get; set; }
+		public string UpdatedBy { get; set; }
 	}
 }
