d40872d20bc525389c93a7f0a1b830c2a61b7441
ZU3AIR <xubairjamil@gmail.com>
2023-09-21 15:27:47 +0500

    Cosmetic changes

--- LocalDropshipping.Web/Middlewares/AuthenticationMiddleware.cs
+++ LocalDropshipping.Web/Middlewares/AuthenticationMiddleware.cs
@@ -23,7 +23,7 @@ namespace LocalDropshipping.Web.Middlewares
                 var user = await _userService.GetCurrentUserAsync();
                 if(user != null)
                 {
-                    context.Session.SetString("CURRENT_USER", JsonConvert.SerializeObject(user));
+                    context.Session.SetString("CurrentUser", JsonConvert.SerializeObject(user));
                     List<Roles> currentUserRoles = new List<Roles>();
                     if (user.IsSuperAdmin)
                     {
